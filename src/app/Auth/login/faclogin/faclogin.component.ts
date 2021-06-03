5e983b914891d14521b717d8d5bd1d3f45ea469e
jensi <jensijahfar293@gmail.com>
2021-06-03 18:22:41 +0530

    first version

---
+++ src/app/Auth/login/faclogin/faclogin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faclogin',
+  templateUrl: './faclogin.component.html',
+  styleUrls: ['./faclogin.component.css']
+})
+export class FacloginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
